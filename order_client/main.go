f85eb6e7cb8700341eedffde5a3b659fd1ab43dd
Colin Newell <colin.newell@gmail.com>
2021-11-27 12:18:26 +0000

    Add timeouts

--- order_client/main.go
+++ order_client/main.go
@@ -3,16 +3,25 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	"google.golang.org/grpc"
 
 	"github.com/colinnewell/grpc-experiment/orders"
 )
 
-const address = "localhost:3003"
+const (
+	address = "localhost:3003"
+	timeout = time.Duration(100 * time.Millisecond)
+)
 
 func main() {
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	conn,
+		err := grpc.Dial(address,
+		grpc.WithInsecure(),
+		grpc.WithBlock(),
+		grpc.WithTimeout(timeout),
+	)
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -24,7 +33,9 @@ func main() {
 		Address:  "London",
 		Notes:    "Deliver to car park",
 	}
-	r, err := client.PlaceOrder(context.TODO(), &o)
+	ctx, cancel := context.WithTimeout(context.TODO(), timeout)
+	r, err := client.PlaceOrder(ctx, &o)
+	cancel()
 	if err != nil {
 		log.Fatalf("Failed to place order: %s", err)
 	}
